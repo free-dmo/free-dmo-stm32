fca5ab7f0ae450f595fb4cc1e83381e18b1915b2
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2024-07-08 23:45:43 -0700

    Automatically build all SKUs

--- .github/auto_build.sh
+++ .github/auto_build.sh
@@ -9,20 +9,48 @@ build_sku () {
   cp build/freedmo.bin freedmo-default-sku-${SKU}.bin
 }
 
+build_sku 11353
+build_sku 30252
+build_sku 30256
+build_sku 30270
+build_sku 30299
+build_sku 30323
+build_sku 30321
+build_sku 30326
+build_sku 30327
+build_sku 30332
+build_sku 30333
+build_sku 30334
+build_sku 30336
+build_sku 30347
+build_sku 30370
+build_sku 30373
+build_sku 30374
+build_sku 30387
+build_sku 30572
+build_sku 30573
+build_sku 30578
+build_sku 30856
+build_sku 30857
+build_sku 1738541
+build_sku 1738595
+build_sku 1744907
+build_sku 1760756
+build_sku 1763982
+build_sku 1933081
+build_sku 1933083
+build_sku 1933084
+build_sku 1933085
+build_sku 1933088
+build_sku 1976411
+build_sku 2133382
+build_sku 2133383
+build_sku S0722400
 build_sku S0722430
 build_sku S0722530
-build_sku S0722550
-build_sku S0722400
-build_sku 1744907
-build_sku 11353
-build_sku 30857
-build_sku 30336
-build_sku 30332
-build_sku 30256
-build_sku 30252
 build_sku S0722540
+build_sku S0722550
 build_sku S0904980
-build_sku 30374
 
 
 DATE=$(date +'%Y%m%d')
